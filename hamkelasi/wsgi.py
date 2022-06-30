5afc0a5c03b1d21949161a1f0d4dcefc10c77341
amir <amirpourastarabadi@gmail.com>
2022-06-30 17:03:49 +0430

    start project

---
+++ hamkelasi/wsgi.py
@@ -0,0 +1,16 @@
+"""
+WSGI config for hamkelasi project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/4.0/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'hamkelasi.settings')
+
+application = get_wsgi_application()
